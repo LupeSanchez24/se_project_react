5774dccb5e1b097f8627aa5f99c17b8982f50ce4
GuadalupeSanchez <guadalupesanchezp6@gmail.com>
2024-08-11 17:27:49 -0700

    New update for my project

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,4 +1,5 @@
 import "./ModalWithForm.css";
+import close from "../../assets/close.svg";
 
 function ModalWithForm() {
   return (
@@ -6,7 +7,7 @@ function ModalWithForm() {
       <div className="modal__content">
         <h2 className="modal__title">New garment</h2>
         <button type="button" className="modal__close">
-          CLOSE
+          <img src={close} alt="close-button" />
         </button>
         <form className="modal__form">
           <label htmlFor="name" className="modal__label">
@@ -28,29 +29,31 @@ function ModalWithForm() {
             ></input>
           </label>
           <fieldset className="modal__radio-buttons">
-            <legend className="modal__legend">Select the weather type;</legend>
+            <legend className="modal__legend">Select the weather type:</legend>
             <label
               htmlFor="hot"
               className=" modal__label modal__label_type_radio"
             >
-              <input id="hot" type="radio" className="modal__radio-input" />
-              hot
+              <input id="hot" type="radio" className="modal__radio-input" /> Hot
             </label>
             <label
               htmlFor="warm"
               className=" modal__label modal__label_type_radio"
             >
               <input id="warm" type="radio" className="modal__radio-input" />{" "}
-              warm
+              Warm
             </label>
             <label
               htmlFor="cold"
               className=" modal__label modal__label_type_radio"
             >
               <input id="cold" type="radio" className="modal__radio-input" />{" "}
-              cold
+              Cold
             </label>
           </fieldset>
+          <button type="submit" className="modal__sumbit">
+            Add garment
+          </button>
         </form>
       </div>
     </div>
