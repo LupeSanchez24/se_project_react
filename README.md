1f56af47c8e6550bf48abfd7325ef4dfcfb41ac5
GuadalupeSanchez <guadalupesanchezp6@gmail.com>
2024-08-13 18:49:04 -0700

    New update for my project

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-# React + Vite
+# React + Vite : WTWR (What to Wear?)
+
+About the Project
+
+The idea of the application is pretty simple - we make a call to an API, which then responds with th daily weather forecast. We collect the weather data, process it, and then based on the forecast, we recommend suitable clothing to the user.
+
+links
+
+https://www.figma.com/design/F03bTb81Pw8IDPj5Y9rc5i/Sprint-10-%7C-WTWR?node-id=568-335&t=ApEQMoS07MlGM1YV-0
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,12 +1,33 @@
 import "./WeatherCard.css";
 
-import cloudy from "../../assets/cloudy.png";
+import { weatherOptions, defaultWeatherOptions } from "../../utils/constants";
 
 function WeatherCard({ weatherData }) {
+  const filteredOptions = weatherOptions.filter((option) => {
+    return (
+      option.day === weatherData.isDay &&
+      option.condition === weatherData.condition
+    );
+  });
+
+  let weatherOption;
+
+  if (filteredOptions.length === 0) {
+    weatherOption = defaultWeatherOptions[weatherData.isDay ? "day" : "night"];
+  } else {
+    weatherOption = filteredOptions[0];
+  }
+
   return (
     <section className="weather-card">
       <p className="weather-card__temp">{weatherData.temp.F} &deg; F </p>
-      <img src={cloudy} alt="cloudy" className="weather-card__image"></img>
+      <img
+        src={weatherOption?.url}
+        alt={`Card showing ${weatherOption?.day ? "day" : "night"}time ${
+          weatherOption?.condition
+        } weather`}
+        className="weather-card__image"
+      ></img>
     </section>
   );
 }
