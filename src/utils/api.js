864998d9570280ff37164dc5e3518f26c2846bf1
GuadalupeSanchez <guadalupesanchezp6@gmail.com>
2024-11-19 12:07:54 -0800

    New update for my project

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,7 @@
-const baseUrl = "http://localhost:3001";
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? " https://api.wtwr-project.jumpingcrab.com"
+    : "http://localhost:3001";
 
 function checkResponse(res) {
   if (res.ok) {
@@ -36,12 +39,4 @@ function deleteItem(item, token) {
   });
 }
 
-
-export {
-  getItems,
-  addNewItem,
-  deleteItem,
-  baseUrl,
-  checkResponse,
-  
-};
+export { getItems, addNewItem, deleteItem, baseUrl, checkResponse };
